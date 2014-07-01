06bba1855ad91dd587c47e8d11252fbd5a33f5ae
xlin <xlin@memphis.edu>
2014-07-01 00:13:59 -0500

    trivial changes in figs

---
+++ src/main/java/evaluations/SimpleWrfExp.java
@@ -0,0 +1,14 @@
+package evaluations;
+
+public class SimpleWrfExp {
+
+	public SimpleWrfExp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
